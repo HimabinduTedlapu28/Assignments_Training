ffa4c6bdabe23b960e49fc02781bf85e3a8dedc3
HimaBindu Tedlapu <himabindute@infinite.com>
2023-10-31 22:41:50 +0530

    commit

---
+++ CSharp_Assignments/RemotingClient/RemotingClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Http;
+//using RemotingServer;
+
+namespace RemotingClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+
---
+++ CSharp_Assignments/assignment-04/assignment-04/account.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_04
+{
+    class account
+    {
+        int Accountno;
+        string CustomerName;
+        string Accounttype;
+        string Transactiontype;
+        int Amount;
+        int balance;
+
+    public account
+            {
+            public int Accountno;
+            public string Customername;
+
+
+            
+            
+
+
+
+
+
+
+        static void Main()
+        {
+            account a = new account();
+            Console.WriteLine("Enter the Accountno :  ");
+            int no = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Customer Name : ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter the Accounttype : ");
+            string type = Console.ReadLine();
+            Console.WriteLine("Enter the Transactiontype : ");
+            string Trans = Console.ReadLine();
+            Console.WriteLine("Enter the amount : ");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the balance : ");
+            int balance = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+    }
+}
